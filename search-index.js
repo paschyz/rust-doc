70d0b9fa5dbd7ce9acf5a6e6b89452a6a5e63017
paschyz <paschyz@users.noreply.github.com>
2024-03-02 15:07:42 +0000

    deploy: 1e9b69e97d7079bb2b4e227a36823f48a41433a8

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["rust_doc",{"doc":"","t":"H","n":["main"],"q":[[0,"rust_doc"]],"d":[""],"i":[0],"f":[[[],1]],"c":[],"p":[[1,"tuple"]],"b":[]}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
