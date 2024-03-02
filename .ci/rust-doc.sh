b7b071daf2ab774aeb1293695a08e0eb4f0459ff
paschyz <paschy2002@gmail.com>
2024-03-02 19:47:21 +0100

    build ci

---
+++ .ci/rust-doc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Echo all commands before executing them
+set -o xtrace
+# Forbid any unset variables
+set -o nounset
+# Exit on any error
+set -o errexit
+
+# Preserve certain files from the gh-pages
+for f in index.html; do
+    cp "gh-pages/$f" ./target/doc/
+done
+
+# Remove everything from gh-pages and replace with the generated docs
+rm -rf gh-pages/*
+cp -R ./target/doc/* gh-pages/
+
+cd gh-pages
+
+# Commit the changes
+git config --global user.email "CI"
+git config --global user.name "CI"
+
+if ! git diff --quiet ; then
+    git add --all
+    git commit --message "CI: cargo doc "
+fi
