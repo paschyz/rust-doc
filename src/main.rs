6cfd8649beeaca15ff3e84efa9b71228d7d90598
paschyz <paschy2002@gmail.com>
2024-03-02 15:59:39 +0100

    build ci

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+/*
+    Close the provided stream
+*/
+fn main() {
+    println!("Hello, world!");
+    // Returns the age of the person.
+    //
+    // # Examples
+    //
+    // ```
+    // let person = Person::new("Eve".to_string(), 35);
+    // assert_eq!(person.age(), 35);
+    // ```
+    println!("Hello, world!");
+}
